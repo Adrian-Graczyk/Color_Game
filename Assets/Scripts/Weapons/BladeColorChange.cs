44d0ddb158d93c527bd5b8e16ea8e23ddf23bba2
KacperSynator <kacper.synator@gmail.com>
2023-06-11 17:29:20 +0200

    BladeColorChange: fix error related to getComponent<Renderer>

--- Assets/Scripts/Weapons/BladeColorChange.cs
+++ Assets/Scripts/Weapons/BladeColorChange.cs
@@ -17,10 +17,11 @@ public class BladeColorChange : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Collider otherCollider = collision.contacts[0].otherCollider;
-        Material otherMaterial = otherCollider.GetComponent<Renderer>().sharedMaterial;
 
         if (otherCollider.CompareTag("ColorObject") || otherCollider.CompareTag("Throwable") && otherCollider.gameObject.layer == LayerMask.NameToLayer("ColorObject"))
         {
+            Material otherMaterial = otherCollider.GetComponent<Renderer>().sharedMaterial;
+
             GetComponent<Renderer>().sharedMaterial = otherMaterial;
         }
     }
