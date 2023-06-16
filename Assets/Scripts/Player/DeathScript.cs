65e9bd3b6f45a726598dfc890837a79fd87a677c
Adrian-Graczyk <252911@student.pwr.edu.pl>
2023-06-17 01:56:38 +0200

    Add death effect

--- Assets/Scripts/Player/DeathScript.cs
+++ Assets/Scripts/Player/DeathScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DeathScript : MonoBehaviour
 {
@@ -8,19 +9,66 @@ public class DeathScript : MonoBehaviour
     public GameEvent onPlayerDeath;
 
     [SerializeField] private Collider playerCollider;
+    [SerializeField] private AudioSource deathSound;
+    [SerializeField] private Volume deathEffect;
+
+    private bool isVolumeChanging = false;
+
     private void OnCollisionEnter(Collision collision)
     {
         GameObject other = collision.contacts[0].otherCollider.gameObject;
 
         if (other.CompareTag("EnemyBullet") || other.CompareTag("EnemyBlade"))
         {
-            onPlayerDeath.Raise(this, null);
-        }
-    }   
+            deathSound.Play();
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("EnemyBlade")) {
-            onPlayerDeath.Raise(this, null);
+            StartCoroutine(ChangeVolumeWeight(0.5f)); // Start the volume change coroutine
+
+            StartCoroutine(WaitForVolumeChangeAndRaiseEvent()); // Wait for the volume change to end and raise the event
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyBlade"))
+        {
+            deathSound.Play();
+
+            StartCoroutine(ChangeVolumeWeight(0.5f)); // Start the volume change coroutine
+
+            StartCoroutine(WaitForVolumeChangeAndRaiseEvent()); // Wait for the volume change to end and raise the event
+        }
+    }
+
+    private IEnumerator ChangeVolumeWeight(float duration)
+    {
+        isVolumeChanging = true;
+        float initialWeight = deathEffect.weight; 
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            deathEffect.weight = Mathf.Lerp(initialWeight, 1, t); 
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        deathEffect.weight = 1;
+        isVolumeChanging = false; 
+    }
+
+    private IEnumerator WaitForVolumeChangeAndRaiseEvent()
+    {
+        while (isVolumeChanging) // Wait until volume change is complete
+        {
+            yield return null;
+        }
+
+        
+        deathEffect.weight = 0f; 
+        onPlayerDeath.Raise(this, null); // Raise the event after volume change is complete
+    }
+
 }
